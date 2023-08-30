b773ce2b27039440c2f5a5f20b315f94999fe2e2
M1izuki <134347712+Tegarpanggalih@users.noreply.github.com>
2023-08-30 14:39:41 +0700

    Create sidebar.php

---
+++ jobsheet 3/app/views/sidebar.php
@@ -0,0 +1,28 @@
+<div class="container-fluid">
+  <div class="row">
+    <nav id="sidebarMenu" class="col-md-2 col-lg-2 d-md-block bg-body-tertiary sidebar collapse">
+      <div class="position-sticky pt-3 sidebar-sticky">
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <a class="nav-link" href= "tampil_mhs.php">
+              <span data-feather="users" class="align-text-bottom"></span>
+              Mahasiswa
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="tampil_dsn.php">
+              <span data-feather="file" class="align-text-bottom"></span>
+              Dosen
+            </a>
+          </li>
+          
+
+        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-body-secondary text-uppercase">
+          <span></span>
+          <a class="link-secondary" href="#" aria-label="Add a new report">
+          </a>
+        </h6>
+        <ul class="nav flex-column mb-2">
+        </ul>
+      </div>
+    </nav>
